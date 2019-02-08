2dad15d4547d176a56525471b6185da0775d72ec
JETZ <jeremyetzine@hotmail.com>
2019-02-08 14:56:54 +1100

    login setup and functioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root :to => 'session#new'
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
+
   resources :seats
   resources :event_spaces
   resources :events
