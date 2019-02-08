e8e34dcd4037d7126c9bb5f0d5951ee54174422d
JETZ <jeremyetzine@hotmail.com>
2019-02-08 14:40:56 +1100

    fixed users!!!!!!!!

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,11 @@ es5 = EventSpace.create :name => 'Playhouse', :capacity => 39
 es6 = EventSpace.create :name => 'Utzon Room', :capacity => 21
 
 
-# User.destroy_all
-# u1 = User.create :name => 'John', :email => 'john@ga.co', :password => 'chicken'
-# u2 = User.create :name => 'Chicken', :email => 'chicken@ga.co', :password => 'chicken'
-# u3 = User.create :name => 'Turkey', :email => 'turkey@ga.co', :password => 'turkey'
-# u4 = User.create :name => 'Duck', :email => 'duck@ga.co', :password => 'duck'
+User.destroy_all
+u1 = User.create :name => 'John', :email => 'john@ga.co', :password => 'chicken'
+u2 = User.create :name => 'Chicken', :email => 'chicken@ga.co', :password => 'chicken'
+u3 = User.create :name => 'Turkey', :email => 'turkey@ga.co', :password => 'turkey'
+u4 = User.create :name => 'Duck', :email => 'duck@ga.co', :password => 'duck'
 
 
 Event.destroy_all
@@ -28,8 +28,8 @@ e3 = Event.create :name => '360 ALLSTARS', :date => '13/4/2019', :duration => 70
 e4 = Event.create :name => 'Verve', :date => '5/4/2019', :duration => 120, :event_space_id => es2.id, :category_id => c1.id
 
 
-# Seat.destroy_all
-# s1 = Seat.create :seat_name => 'Stalls', :seat_num => 50, :event_space_id => es1.id, :user_id => u1.id
-# s2 = Seat.create :seat_name => 'Circle', :seat_num => 111, :event_space_id => es2.id, :user_id => u2.id
-# s3 = Seat.create :seat_name => 'Main', :seat_num => 25, :event_space_id => es4.id, :user_id => u3.id
-# s4 = Seat.create :seat_name => 'Stalls', :seat_num => 24, :event_space_id => es2.id, :user_id => u4.id
+Seat.destroy_all
+s1 = Seat.create :seat_name => 'Stalls', :seat_num => 50, :event_space_id => es1.id, :user_id => u1.id
+s2 = Seat.create :seat_name => 'Circle', :seat_num => 111, :event_space_id => es2.id, :user_id => u2.id
+s3 = Seat.create :seat_name => 'Main', :seat_num => 25, :event_space_id => es4.id, :user_id => u3.id
+s4 = Seat.create :seat_name => 'Stalls', :seat_num => 24, :event_space_id => es2.id, :user_id => u4.id
