8c0754809c8da92a2173582328694a90f40bc4fb
Brendan Shields <brendan.m.j.shields@gmail.com>
2019-02-14 10:56:17 +1100

    hundreds of seats added to seed data

--- app/views/seats/_seat.json.jbuilder
+++ app/views/seats/_seat.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! seat, :id, :seat_num, :section_id
+json.extract! seat, :id, :cx, :cy, :r, :seat_num, :section_id
 json.url seat_url(seat, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_021900) do
+ActiveRecord::Schema.define(version: 2019_02_13_183659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,9 @@ ActiveRecord::Schema.define(version: 2019_02_13_021900) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "section_id"
+    t.float "cx"
+    t.float "cy"
+    t.float "r"
   end
 
   create_table "sections", force: :cascade do |t|
