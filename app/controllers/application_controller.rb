8a23c803b9217866a353d83f7138c68bc68f5dd1
JETZ <jeremyetzine@hotmail.com>
2019-02-15 09:50:02 +1100

    cleaned some code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
 class ApplicationController < ActionController::API
-  # skip_before_action :verify_authenticity_token
-  # before_action :fetch_user
-  #
-  # private
-  # def fetch_user
-  #   @current_user = User.find_by :id => session[:user_id] if session[:user_id].present?
-  #   session[:user_id] = nil unless @current_user.present?
-  # end
-  #
-  # def check_for_login
-  #   redirect_to login_path unless @current_user.present?
-  # end
-
   # Include Knock within your application.
   include Knock::Authenticable
 
