eed2614f8bf9d4d15ec9f5d11b28a6a06e9be6b6
JETZ <jeremyetzine@hotmail.com>
2019-02-14 16:20:43 +1100

    added photos, short&long desc and subtitle for the events

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true # ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "operahouse_server_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "NBA_app_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_005208) do
+ActiveRecord::Schema.define(version: 2019_02_14_035301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,10 @@ ActiveRecord::Schema.define(version: 2019_02_14_005208) do
     t.datetime "updated_at", null: false
     t.integer "category_id"
     t.integer "event_space_id"
+    t.text "image"
+    t.text "subtitle"
+    t.text "short_desc"
+    t.text "long_desc"
   end
 
   create_table "seats", force: :cascade do |t|
